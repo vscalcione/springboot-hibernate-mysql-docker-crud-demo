af70ab1e57450e27dbd17bfb8dce1418a5fd3431
vscalcione <vincenzo99.scalcione@gmail.com>
2021-09-05 21:39:28 +0200

    Update EmployeeController.java

---
+++ src/main/java/it/vscalcione/springboot/demoapplication/controller/EmployeeController.java
@@ -0,0 +1,46 @@
+package it.vscalcione.springboot.demoapplication.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/employees")
+public class EmployeeController {
+	
+	private EmployeeService employeeService;
+
+	public EmployeeController(EmployeeService employeeService) {
+		super();
+		this.employeeService = employeeService;
+	}
+	
+	@PostMapping()
+	public ResponseEntity<Employee> saveEmployee(@RequestBody Employee employee) {
+		return new ResponseEntity<Employee>(employeeService.saveEmployee(employee), HttpStatus.CREATED);
+	}
+	
+	@GetMapping()
+	public List<Employee> getAllEmployees() {
+		return employeeService.getAllEmployees();
+	}
+	
+	@GetMapping("{id}")
+	public ResponseEntity<Employee> getEmployeeById(@PathVariable("id") Long id) {
+		return new ResponseEntity<Employee>(employeeService.getEmployeeById(id), HttpStatus.OK);
+	}
+	
+	@PutMapping("{id}")
+	public ResponseEntity<Employee> updateEmployee(@PathVariable("id") Long id, @RequestBody Employee employee) {
+		return new ResponseEntity<Employee>(employeeService.updateEmployee(employee, id), HttpStatus.OK);
+	}
+	
+	@DeleteMapping("{id}")
+	public ResponseEntity<String> deleteEmployee(@PathVariable("id") Long id) {
+		employeeService.deleteEmployee(id);
+		return new ResponseEntity<String>("Employee deleted with success.", HttpStatus.OK);
+	}
+}
